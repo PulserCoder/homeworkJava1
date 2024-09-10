107636f5369ad11ad3a04affb3bcbb69796d7cf3
PulserCoder <92484659+PulserCoder@users.noreply.github.com>
2024-09-10 18:21:03 +0300

    done homework, completed first 8 tasks

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Hi, this is my first project which has written by Java, I hope I will learn a lot of interesting things
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        var dog1 = dog + 4;
+        var cat1 = cat + 4;
+        var paper1 = paper + 4;
+        System.out.println(dog1);
+        System.out.println(cat1);
+        System.out.println(paper1);
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog *= 10;
+        System.out.println(frog);
+        frog /= 3.5;
+        System.out.println(frog);
+        frog += 4;
+        System.out.println(frog);
+        var massOfBoxer1 = 78.2;
+        var massOfBoxer2 = 82.7;
+        System.out.println(massOfBoxer1 + massOfBoxer2);
+        System.out.println(massOfBoxer2 - massOfBoxer1);
+        System.out.println(massOfBoxer2 % massOfBoxer1);
+        var commonHours = 640;
+        var quanityOfWorkers = commonHours / 8;
+        System.out.println("Всего работников в компании — " + quanityOfWorkers + " человек");
+        quanityOfWorkers += 94;
+        commonHours = quanityOfWorkers * 8;
+        System.out.println("Если в компании работает " + quanityOfWorkers + " человек, то всего " + commonHours + " часов работы может быть поделено между сотрудниками");
+
+
+    }
+}
+
+
+
