795ee25025ce149c480006f3de2487bb9e85514a
PulserCoder <92484659+PulserCoder@users.noreply.github.com>
2024-09-17 22:07:20 +0300

    done 9 honework

--- src/Main.java
+++ src/Main.java
@@ -4,64 +4,39 @@ import java.util.StringJoiner;
 public class Main {
     public static void main(String[] args) {
         //task1
-        int[] mTask1 = new int[3];
-        mTask1[0] = 1;
-        mTask1[1] = 2;
-        mTask1[2] = 3;
-        float[] mTask1_2 = {1.57f, 7.654f, 9.986f};
-        int[] m3 = {4, 5, 6};
-        //task2
-        System.out.print(mTask1[0]);
-        for (int i = 1; i < mTask1.length; i++) {
-            System.out.print(", ");
-            System.out.print(mTask1[i]);
+        int[] spendings = new int[]{1000, 25000, 21000, 19000, 5500};
+        int total = 0;
+        for (int i = 0; i < spendings.length; i++) {
+            total += spendings[i];
         }
-        System.out.print("\n");
-        System.out.print(mTask1_2[0]);
-        for (int i = 1; i < mTask1_2.length; i++) {
-            System.out.print(", ");
-            System.out.print(mTask1_2[i]);
-        }
-        System.out.print("\n");
-        System.out.print(m3[0]);
-        for (int i = 1; i < m3.length; i++) {
-            System.out.print(", ");
-            System.out.print(m3[i]);
-        }
-
-        //task3
-        System.out.print("\n");
-        System.out.print(mTask1[mTask1.length - 1]);
-        for (int i = 1; i > -1; i--) {
-            System.out.print(", ");
-            System.out.print(mTask1[i]);
-        }
-        System.out.print("\n");
-        System.out.print(mTask1_2[mTask1_2.length - 1]);
-        for (int i = 1; i > -1; i--) {
-            System.out.print(", ");
-            System.out.print(mTask1_2[i]);
-        }
-        System.out.print("\n");
-        System.out.print(m3[m3.length - 1]);
-        for (int i = 1; i > -1; i--) {
-            System.out.print(", ");
-            System.out.print(m3[i]);
-        }
-        System.out.print("\n");
-        //task4
-        for (int i = 0; i < mTask1.length; i++) {
-            if (mTask1[i] % 2 != 0) {
-                mTask1[i] += 1;
+        System.out.printf("Сумма трат за месяц составила %s рублей\n", total);
+        //task 2
+        int[] spendings2 = new int[]{1000, 25000, 21000, 19000, 5500};
+        int minn = Integer.MAX_VALUE;
+        int maxn = Integer.MIN_VALUE;
+        for (int i = 0; i < spendings2.length; i++) {
+            if (spendings2[i] < minn) {
+                minn = spendings2[i];
+            }
+            if (spendings2[i] > maxn) {
+                maxn = spendings2[i];
             }
         }
-        for (int i = 0; i < mTask1.length; i++) {
-            System.out.println(mTask1[i]);
+        System.out.printf("Минимальная сумма трат за неделю составила %s рублей. Максимальная сумма трат за неделю составила %s рублей\n", minn, maxn);
+        //task3
+        int[] spendings3 = new int[]{1000, 25000, 21000, 19000, 5500};
+        int sum = 0;
+        for (int i = 0; i < spendings3.length; i++) {
+            sum += spendings3[i];
         }
-        // or
-        System.out.println(Arrays.toString(mTask1));
-
+        System.out.printf("Средняя сумма трат за месяц составила %s рублей\n", sum / (float) spendings3.length);
+        //task 4
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i > -1; i--) {
+            System.out.printf(Character.toString(reverseFullName[i]));
+        }
     }
+
 }
 
 
