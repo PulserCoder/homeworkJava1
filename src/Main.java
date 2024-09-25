26506df1c4302f3d592228690b66009bbdd92e5b
PulserCoder <92484659+PulserCoder@users.noreply.github.com>
2024-09-25 10:47:08 +0300

    done 10 honework

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,17 @@ import java.util.StringJoiner;
 
 public class Main {
     public static void main(String[] args) {
-        //task1
-        int[] spendings = new int[]{1000, 25000, 21000, 19000, 5500};
-        int total = 0;
-        for (int i = 0; i < spendings.length; i++) {
-            total += spendings[i];
-        }
-        System.out.printf("Сумма трат за месяц составила %s рублей\n", total);
+        //task 1
+        String firstName = "Ivan";
+        String lastName = "Ivanov";
+        String middleName = "Ivanovich";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
         //task 2
-        int[] spendings2 = new int[]{1000, 25000, 21000, 19000, 5500};
-        int minn = Integer.MAX_VALUE;
-        int maxn = Integer.MIN_VALUE;
-        for (int i = 0; i < spendings2.length; i++) {
-            if (spendings2[i] < minn) {
-                minn = spendings2[i];
-            }
-            if (spendings2[i] > maxn) {
-                maxn = spendings2[i];
-            }
-        }
-        System.out.printf("Минимальная сумма трат за неделю составила %s рублей. Максимальная сумма трат за неделю составила %s рублей\n", minn, maxn);
-        //task3
-        int[] spendings3 = new int[]{1000, 25000, 21000, 19000, 5500};
-        int sum = 0;
-        for (int i = 0; i < spendings3.length; i++) {
-            sum += spendings3[i];
-        }
-        System.out.printf("Средняя сумма трат за месяц составила %s рублей\n", sum / (float) spendings3.length);
-        //task 4
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i > -1; i--) {
-            System.out.printf(Character.toString(reverseFullName[i]));
-        }
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        //task 3
+        String fullName2 = "Иванов Семён Семёнович";
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName2.replace('ё', 'е'));
     }
 
 }
